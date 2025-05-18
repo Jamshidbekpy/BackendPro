ee078e38ba9b53dc5dcf711614f7f1026b37d07e
Jamshidbek Shodibek <159956769+Jamshidbekpy@users.noreply.github.com>
2025-05-19 00:11:08 +0500

    beautiful

--- default/tests/views.py
+++ default/tests/views.py
@@ -11,6 +11,7 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_str    
 from django.core.mail import EmailMessage
 from .serializers import RegisterSerializer
+from django.shortcuts import HttpResponse
 
 User = get_user_model()
 
@@ -34,7 +35,7 @@ class RegisterView(GenericAPIView):
                 <h2>Welcome to Our Site!</h2>
                 <p>Hi <strong>{user.username}</strong>,</p>
                 <p>Thank you for registering. Please activate your account by clicking the link below:</p>
-                <p><a href="http://{current_site.domain}/tests/activate/{uid}/{token}/" style="padding:10px 20px; background-color:#4CAF50; color:white; text-decoration:none; border-radius:5px;">
+                <p><a href="http://192.168.125.220:8000/tests/activate/{uid}/{token}/" style="padding:10px 20px; background-color:#4CAF50; color:white; text-decoration:none; border-radius:5px;">
                 Activate Account</a></p>
                 <p>If you did not register, please ignore this email.</p>
                 <br>
@@ -61,7 +62,41 @@ class ActivationView(APIView):
         if user is not None and default_token_generator.check_token(user, token):
             user.is_active = True
             user.save()
-            return Response({'message': 'Activation successful'}, status=status.HTTP_200_OK)
+            return HttpResponse(""" <!DOCTYPE html>
+    <html lang="uz">
+    <head>
+        <meta charset="UTF-8">
+        <title>Welcome</title>
+        <style>
+            body {
+                background: linear-gradient(to right, #74ebd5, #ACB6E5);
+                font-family: Arial, sans-serif;
+                text-align: center;
+                padding-top: 100px;
+                color: #333;
+            }
+            h1 {
+                font-size: 48px;
+                color: #2c3e50;
+                margin-bottom: 20px;
+            }
+            p {
+                font-size: 20px;
+                color: #34495e;
+                max-width: 600px;
+                margin: 0 auto;
+                background-color: rgba(255, 255, 255, 0.8);
+                padding: 20px;
+                border-radius: 10px;
+                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Assalomu alaykum!</h1>
+        <p>Akkauntingiz muvaffaqiyatli aktivlashtirildi.</p>
+    </body>
+    </html>""")
         else:
             return Response({'message': 'Activation link is invalid'}, status=status.HTTP_400_BAD_REQUEST)
 
